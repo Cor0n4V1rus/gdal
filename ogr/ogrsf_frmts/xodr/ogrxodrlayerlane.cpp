4a67ba69f0b6510b0ebeb111deac04526136205d
Cor0n4V1rus <60296087+Cor0n4V1rus@users.noreply.github.com>
2026-09-24 18:19:22 +0200

    Update ogrxodrlayerlane.cpp
    Fixing
    
    ./gdal-3.13.3/Source/ogr/ogrsf_frmts/xodr/ogrxodrlayerlane.cpp: In member function 'OGRFeature* OGRXODRLayerLane::GetNextRawFeature()':
    ./gdal-3.13.3/Source/ogr/ogrsf_frmts/xodr/ogrxodrlayerlane.cpp:123:26: error: no matching function for call to 'OGRFeature::SetField(int, std::optional<int>&)'
      123 |         feature->SetField(m_poFeatureDefn->GetFieldIndex("Predecessor"),
          |         ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      124 |                           lane.predecessor);
          |                           ~~~~~~~~~~~~~~~~~
      * there are 23 candidates
    In file included from ./gdal-3.13.3/Source/ogr/ogrsf_frmts/ogrsf_frmts.h:18,
                     from ./gdal-3.13.3/Source/ogr/ogrsf_frmts/xodr/ogr_xodr.h:14,
                     from ./gdal-3.13.3/Source/ogr/ogrsf_frmts/xodr/ogrxodrlayerlane.cpp:16:
        * candidate 1: 'void OGRFeature::SetField(const char*, int)'
          ./gdal-3.13.3/Source/ogr/ogr_feature.h:1579:10:
           1579 |     void SetField(const char *pszFName, int nValue)
                |          ^~~~~~~~
          * no known conversion for argument 2 from 'std::optional<int>' to 'int'
            ./gdal-3.13.3/Source/ogr/ogr_feature.h:1579:45:
             1579 |     void SetField(const char *pszFName, int nValue)
                  |                                         ~~~~^~~~~~
        * candidate 2: 'void OGRFeature::SetField(const char*, GIntBig)'
          ./gdal-3.13.3/Source/ogr/ogr_feature.h:1584:10:
           1584 |     void SetField(const char *pszFName, GIntBig nValue)
                |          ^~~~~~~~
          * no known conversion for argument 2 from 'std::optional<int>' to 'GIntBig' {aka 'long long int'}
            ./gdal-3.13.3/Source/ogr/ogr_feature.h:1584:49:
             1584 |     void SetField(const char *pszFName, GIntBig nValue)
                  |                                         ~~~~~~~~^~~~~~
    ...
    
    compile error; assuming that '0' is a sane default value ...

--- ogr/ogrsf_frmts/xodr/ogrxodrlayerlane.cpp
+++ ogr/ogrsf_frmts/xodr/ogrxodrlayerlane.cpp
@@ -120,10 +120,28 @@ OGRFeature *OGRXODRLayerLane::GetNextRawFeature()
         feature->SetField(m_poFeatureDefn->GetFieldIndex("LaneID"), lane.id);
         feature->SetField(m_poFeatureDefn->GetFieldIndex("Type"),
                           lane.type.c_str());
-        feature->SetField(m_poFeatureDefn->GetFieldIndex("Predecessor"),
-                          lane.predecessor);
-        feature->SetField(m_poFeatureDefn->GetFieldIndex("Successor"),
-                          lane.successor);
+        
+        if ( lane.predecessor.has_value () )
+        {
+            feature->SetField(m_poFeatureDefn->GetFieldIndex("Predecessor"),
+                            lane.predecessor.value());
+        }
+        else
+        {
+            feature->SetField(m_poFeatureDefn->GetFieldIndex("Predecessor"),
+                              0);
+        }
+
+        if ( lane.successor.has_value () )
+        {
+            feature->SetField(m_poFeatureDefn->GetFieldIndex("Successor"),
+                            lane.successor.value());
+        }
+        else
+        {
+            feature->SetField(m_poFeatureDefn->GetFieldIndex("Successor"),
+                              0);
+        }
 
         ++m_laneIter;
         ++m_laneMeshIter;
